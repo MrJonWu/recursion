e5e5c9e360925402096f3053cd8842bb40d675c8
MrJonWu <mrjonwu@gmail.com>
2016-10-21 03:16:26 -0700

    Make the getElementsByClassName function work via recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+/*
+NOTES: "You should use document.body, element.childNodes, and element.classList" - Hack Reactor Instructions
+
+document.body : Returns the <body> element of the current document.
+element.classList : Returns a DOMTokenList containing the list of class attributes.
+	- DOMTokenList.contains() - Returns true if the underlying string contains token, otherwise false
+Tip: To return a collection of a node's element nodes (excluding text and comment nodes), use the children property. eg. document.body.children;
+*/
+
+var getElementsByClassName = function(className, node, matches) {
+  var matches = (matches === undefined ? [] : matches);
+  var currentNode = (node === undefined ? document.body : node);
+
+  //Check the current node's class for a match 
+  if (currentNode.classList.contains(className)){
+  	matches.push(currentNode);
+  }
+  //check each of the children of the currentNode via recursion
+  for (var i=0; i<currentNode.children.length; i++) {
+  	getElementsByClassName(className, currentNode.children[i], matches);
+  }
+  return matches;
 };
+
