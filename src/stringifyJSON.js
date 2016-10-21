643c53640bf0a412f371f597b851dfcacba17a91
MrJonWu <mrjonwu@gmail.com>
2016-10-21 16:30:12 -0700

    Get stringifyJSON.js working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,55 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+/* NOTES: 
+	Use "typeof" on each item of the array to determine how to deal with it 
+		typeof Undefined == "undefined"
+		typeof Null == "object"
+		typeof Boolean == "boolean"
+		typeof Number == "number"
+		typeof String == "string"
+		typeof Symbol == "symbol"
+		typeof Function == "function"
+		typeof Any other object == "object"
+	// use Array.isArray or Object.prototype.toString.call
+	// to differentiate regular objects from arrays
+*/
 var stringifyJSON = function(obj) {
-  // your code goes here
+    if (typeof obj !== "object") { 
+		//if not object/null/array
+		if (typeof obj !== "string") {
+			return obj.toString();
+		} else {
+			//put double quotes around string
+			return '"'+obj+'"';
+		}
+	//if function is null/array/or object	
+	} else if (typeof obj === "object"){
+		//if null
+		if (obj === null) {
+			return 'null';
+		} 
+		//if array	
+		if(Array.isArray(obj)) {
+			var JSONedArray = [];
+			obj.forEach(function(element){
+				JSONedArray.push(stringifyJSON(element));
+			})
+			return '['+JSONedArray.toString()+']';
+		//if object
+		} else if (Object.prototype.toString.call(obj) === '[object Object]'){
+			var JSONedArray = [];
+			var JSONedObj = {};
+			for (var key in obj){
+				if (typeof obj[key] === "function" || typeof obj[key] === "undefined"){
+					//do nothing if the value in an object is a function or undefined
+				} else {
+					var JSONKey = stringifyJSON(key);
+					var JSONValue = stringifyJSON(obj[key])
+					JSONedArray.push(JSONKey+':'+JSONValue);
+				}
+			}
+			return '{'+JSONedArray.toString()+'}';
+		}
+	}
 };
